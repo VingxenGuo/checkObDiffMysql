8fb840dbdbb12eab3dc5801ef1e97dc6ffe765c3
vingxen <vingxen@cloud-miner.net>
2025-02-14 16:21:09 +0800

    refactor: update configuration and output format

--- checkSqlDiff.py
+++ checkSqlDiff.py
@@ -1,22 +1,33 @@
 import mysql.connector
 from datetime import datetime
+import configparser
+import os
 
-# 需要確認差異的物件
-check_ob = 'routine' # 不用加 s
+# 讀取設定檔
+config = configparser.ConfigParser()
+config.read('setting.config')
+
+# 從設定檔讀取參數
+check_ob = config['check']['check_ob']
 
 # A DB 連線資訊
-db_name_A  = 'game-dev' # 此項僅影響輸出名稱，方便結果辨識
-host_A = '54.169.249.219'
-port_A = 3306
-user_A = 'root'
-password_A = '!QAZ2wsx'
+db_name_A = config['db_a']['db_name']
+host_A = config['db_a']['host']
+port_A = int(config['db_a']['port'])
+user_A = config['db_a']['user']
+password_A = config['db_a']['password']
 
 # B DB 連線資訊
-db_name_B = 'kx-merge-s' # 此項僅影響輸出名稱，方便結果辨識
-host_B = '35.220.144.182'
-port_B = 3306
-user_B = 'root'
-password_B = ')>x(6isS;9,=}S@:'
+db_name_B = config['db_b']['db_name']
+host_B = config['db_b']['host']
+port_B = int(config['db_b']['port'])
+user_B = config['db_b']['user']
+password_B = config['db_b']['password']
+
+# 建立輸出資料夾
+output_folder = f"diff_{db_name_A}_{db_name_B}"
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
 
 # 要撈的物件 query
 if check_ob == 'view':
@@ -29,12 +40,16 @@ connection = mysql.connector.connect(host=host_A, port=port_A, user=user_A, pass
 cursor = connection.cursor()
 cursor.execute(query)
 data = cursor.fetchall()
+cursor.close()
+connection.close()
 
 # 建立 B 連線
 connection = mysql.connector.connect(host=host_B, port=port_B, user=user_B, password=password_B)
 cursor = connection.cursor()
 cursor.execute(query)
 data_2 = cursor.fetchall()
+cursor.close()
+connection.close()
 
 # 將 list 設為 set 來檢查不一樣的
 set1 = set(data)
@@ -43,6 +58,25 @@ set2 = set(data_2)
 diff1 = set1 - set2
 diff2 = set2 - set1
 
-print(db_name_A, "有", db_name_B, " 沒有的", check_ob, " : ", diff1)
-print("#" * 100)
-print(db_name_B, "有", db_name_A, " 沒有的", check_ob, " : ", diff2)
+# 將結果寫入檔案
+output_file = os.path.join(output_folder, f"diff_{check_ob}.txt")
+with open(output_file, 'w', encoding='utf-8') as f:
+    # 第一部分的差異
+    f.write(f"{db_name_A} 有 {db_name_B} 沒有的 {check_ob} 共 {len(diff1)} 個 : \n")
+    if diff1:
+        formatted_diff1 = ',\n '.join(str(item) for item in sorted(diff1))
+        f.write(f"{{\n {formatted_diff1}}}\n")
+    else:
+        f.write("{}\n")
+    
+    f.write("#" * 100 + "\n")
+    
+    # 第二部分的差異
+    f.write(f"{db_name_B} 有 {db_name_A} 沒有的 {check_ob} 共 {len(diff2)} 個 : \n")
+    if diff2:
+        formatted_diff2 = ',\n '.join(str(item) for item in sorted(diff2))
+        f.write(f"{{\n {formatted_diff2}}}\n")
+    else:
+        f.write("{}\n")
+
+print(f"比對結果已輸出至 {output_file}")
